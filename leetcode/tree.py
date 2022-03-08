9ff5962c6f9e72641ee3f43ff7ed30ba322f9074
alina1412 <alina1412@mail.ru>
2022-03-08 15:44:17 +0300

    +comments

--- leetcode/tree.py
+++ leetcode/tree.py
@@ -1,3 +1,7 @@
+# The auxiliary TreeNode class from [LeetCode site]
+# (https://leetcode.com/playground/new/linked-list/)
+# for the demonstrational task.
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
